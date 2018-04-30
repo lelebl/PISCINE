0fde562988297d86b425a37d4288cddb25287366
EmmanuelleTHRLX <emmanuelle.thiroloix@gmail.com>
2018-04-30 13:31:21 +0200

    Création page connexion & header.inc

---
+++ include/header.php
@@ -0,0 +1,23 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Emmanuelle Thiroloix, Léa Blanchard, Charlotte Decary">
+    <link rel="favicon" href="images/favicon.png">
+
+    <title><?php echo $title ?></title>
+
+    <!--Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+</head>
---
+++ login.php
@@ -0,0 +1,70 @@
+<?php 
+$title="Login";
+include("include/header.php"); 
+?>
+
+  <body class="login-body">
+
+    <div class="container">
+
+      <form class="form-signin" action="index.html">
+        <h2 class="form-signin-heading">Connecte toi!</h2>
+        <div class="login-wrap">
+            <div class="user-login-info">
+                <input type="text" class="form-control" placeholder="Pseudo" autofocus>
+                <input type="password" class="form-control" placeholder="Mot de passe">
+            </div>
+            <label class="checkbox">
+                <input type="checkbox" value="remember-me"> Mémoriser
+                <span class="pull-right">
+                    <a data-toggle="modal" href="#myModal"> Mot de passe oublié?</a>
+
+                </span>
+            </label>
+            <button class="btn btn-lg btn-login btn-block" type="submit"> Enregistrement </button>
+
+            <div class="registration">
+                Pas encore de compte?
+                <a class="" href="enregistrement.php">
+                    Créer mon compte
+                </a>
+            </div>
+
+        </div>
+
+          <!-- Modal -->
+          <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                          <h4 class="modal-title">Mot de passe oublié ?</h4>
+                      </div>
+                      <div class="modal-body">
+                          <p>Etrez votre email avant de renouveler votre mot de passe</p>
+                          <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+
+                      </div>
+                      <div class="modal-footer">
+                          <button data-dismiss="modal" class="btn btn-default" type="button">Supprimer</button>
+                          <button class="btn btn-success" type="button">Valider</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!-- modal -->
+
+      </form>
+
+    </div>
+
+
+
+    <!-- Placed js at the end of the document so the pages load faster -->
+
+    <!--Core js-->
+    <script src="js/jquery.js"></script>
+    <script src="bs3/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
