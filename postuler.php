e0662f6ab44b57ecb99b106a902d4a36a7b0e1d7
Léa Blanchard <lea10.blanchard@gmail.com>
2018-05-01 15:40:00 +0200

    postuler

---
+++ postuler.php
@@ -0,0 +1,31 @@
+	<?php
+	$title="Offre d'emploi";
+	include("include/header.inc.php"); 
+	include("include/menu_haut.inc.php"); 
+	include("include/menu_gauche.inc.php"); 
+
+
+	?>
+
+	<body>
+	
+	<section id="main-content">
+		<section class="wrapper">
+			<div class="row">
+				<div class="col-lg-12"> 
+					<section class="panel"><header class="panel-heading">LES OFFRES D'EMPLOI</header></section>
+				</div>
+		</div>
+
+	</section>
+	</section>
+
+
+
+
+	  <!-- Placed js at the end of the document so the pages load faster -->
+
+	    <!--Core js-->
+	    <?php include('include/js.inc.php'); ?>
+
+	</body>
