bc5694467e32a4c5df6b57bef0ab0fdcb6f1340b
Léa Blanchard <lea10.blanchard@gmail.com>
2018-05-01 15:59:12 +0200

    postuler

--- postuler.php
+++ postuler.php
@@ -12,12 +12,345 @@
 	<section id="main-content">
 		<section class="wrapper">
 			<div class="row">
+                <div class="col-md-12">
+                    <!--breadcrumbs start -->
+                    <ul class="breadcrumb">
+                        <li><a href="accueil.php"><i class="fa fa-home"></i> Accueil</a></li>
+                        <li class="active">Vos offres d'emplois </li>
+                    </ul>
+                    <!--breadcrumbs end -->
+                </div>
+            </div>
+			<div class="row">
 				<div class="col-lg-12"> 
-					<section class="panel"><header class="panel-heading">LES OFFRES D'EMPLOI</header></section>
+					<section class="panel"><header class="panel-heading">OFFRES D'EMPLOI</header></section>
 				</div>
-		</div>
+			</div>
 
-	</section>
+
+			<div class="row"></div>
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			</div>
+
+			<div class="row"></div>
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			</div>
+
+
+
+			<div class="row"></div>
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			</div>
+
+
+
+
+			<div class="row"></div>
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			</div>
+
+
+
+
+			<div class="row"></div>
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			
+				<div class="col-lg-4"> 
+					<div class="panel panel-default">
+                   
+					<div class="panel-heading">Intitulé de l'offre 
+					 <span class="tools pull-right">
+                                <a href="javascript:;" class="fa fa-bookmark" ></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                     </span>
+                     </div>
+
+                     <div class="panel-body">
+                     	
+                     	<p> Descripion de l'offre <br><br><br><br><br><br></p>
+                     	<center><button class="btn btn-primary"> POSTULER ! </button></center>
+
+
+                     </div>
+                 </div>
+				</div>
+			</div>
+
+
+
+			</div>
+
+		</section>
 	</section>
 
 
