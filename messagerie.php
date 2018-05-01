1530fa7195942464dd56ad3639a5dc9f403835f2
EmmanuelleTHRLX <emmanuelle.thiroloix@gmail.com>
2018-05-01 10:56:46 +0200

    Messagerie / Menu Gauche

---
+++ messagerie.php
@@ -0,0 +1,12 @@
+<?php 
+$title="Messagerie";
+include("include/header.inc.php"); 
+
+?>
+
+<body>
+ <!--Core js-->
+    <script src="js/jquery.js"></script>
+    <script src="bs3/js/bootstrap.min.js"></script>
+
+  </body>
