7e2ee6aa418ab3c54c91e0eccebadb5d847ef864
Léa Blanchard <lea10.blanchard@gmail.com>
2018-05-01 13:34:49 +0200

    offre d'emploi

--- offre_emploi.php
+++ offre_emploi.php
@@ -21,48 +21,25 @@ include("include/menu_gauche.inc.php");
             <input type="text" class="form-control" placeholder="Salaire">
             <input type="text" class="form-control" placeholder="Secteur">
             <input type="text" class="form-control" placeholder="Langues exigées">
-            
+            <br>
 
             <p>Profil recherché</p>
              <input type="text" class="form-control" placeholder="Diplomes">
              <input type="text" class="form-control" placeholder="Qualités">
-             <input type="text" class="form-control" placeholder="Avantages">
-             <input type="text" class="form-control" placeholder="Avantages">
+            <input type="text" class="form-control" placeholder="Exigences (lettre de motivation, permis etc.)" autofocus>
 
             <br>
-            <br>
-            <p> Personne à contacter </p>
+            <p> Vos coordonnées </p>
             <input type="text" class="form-control" placeholder="Nom" autofocus>
             <input type="text" class="form-control" placeholder="Prénom" autofocus>
             <input type="text" class="form-control" placeholder="Email" autofocus>
-            <input type="date" class="form-control" placeholder="Date" autofocus >
             <br>
-            <input type="text" class="form-control" placeholder="Addresse" autofocus>
-            <input type="text" class="form-control" placeholder="Ville" autofocus >
-            <input type="number" class="form-control" placeholder="Code postal" min="0" max="99999" autofocus >
-            <br>
-            <select name="pays" class="form-control" id="pays"placeholder="Pays">
-                <option value="France">France</option>      
-                <option value="Angleterre">Angleterre</option>  
-                <option value="Irlande">Irlande</option>
-                <option value="Espagne">Espagne</option>
-            </select>
-
           
        
 
+            <button class="btn btn-lg btn-login btn-block" type="submit">Mettre en ligne</button>
 
-           <label class="checkbox">
-                <input type="checkbox" value="agree this condition"> J'accepte les termes et conditions
-            </label>
-            <button class="btn btn-lg btn-login btn-block" type="submit">Valider</button>
-
-            <div class="registration">
-                Connectez-vous :
-                <a class="" href="index.php">
-                    Login
-                </a>
-            </div>
+            
 
         </div>
 
