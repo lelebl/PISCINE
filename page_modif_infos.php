f6db3339c6b753c514735e5ecb0c0b0d0616775a
charlotte decary <charlottedecary@MacBook-de-charlotte-2.local>
2018-05-02 16:01:27 +0200

    page de modification des infos perso
    quelques changements

--- page_modif_infos.php
+++ page_modif_infos.php
@@ -1,39 +1,52 @@
 <?php 
-$title="Modification Informations";
+$title="vrai modification information";
 include("include/header.inc.php"); 
 include("include/menu_haut.inc.php");
 include("include/menu_gauche.inc.php");
-
 ?>
 
 <body>
-	<section id="main-content">
-        <section class="wrapper">
-        <!-- page start-->
-
-        <div class="row">
+    <!--main content start-->
+    <section id="main-content" >
+    <section class="wrapper">
+            <div class="row">
                 <div class="col-md-12">
                     <!--breadcrumbs start -->
                     <ul class="breadcrumb">
-                        <li><a href="accueil.php"><i class="fa fa-home"></i> Profil</a></li>
-                        <li class="active">Modification de mes informations personelles</li>
+                        <li><a href="accueil.php"><i class="fa fa-home"></i> Accueil</a></li>
+                        <li class="active">Modification Informations Personnelles</li>
                     </ul>
                     <!--breadcrumbs end -->
                 </div>
             </div>
             <div class="row">
+                
+                
+            
+                <div class="col-lg-12">
+                        <section class="panel">
+                          <div class="panel-body profile-information">
 
-              <div class="col-lg-12">
-                <section class="panel">
-                  <header class="panel-heading">
-                    Modifiez vos informations personelles
-                    </header>
-                     <div class="panel-body">
-                     	<center>
-                     	<div class="col-lg-6">
-                     		
-     <table class="table">
-                            
+
+                              <div class="col-lg-4">
+                                  <div class="profile-pic text-center">
+                                    <img src="images/emmanuelle.jpg" alt=""/>
+                                  </div>
+                              </div>
+                              <div class="col-lg-1"></div>
+
+                              <div class="col-lg-4">
+                                
+                                  <h1>Prenom Nom</h1>
+                                  <span class="text-muted">Ecole</span>
+                                  <p>
+                                    Compétences: Maths, Anglais, Francais, Power point, ect 
+                                  </p>
+                                
+                              </div>
+                              <div class="col-lg-10">
+                                <table class="table">
+                            <br><br>
                             <tr>
                                 <td><a href="#">NOM</a></td>
                                 <td>
@@ -57,7 +70,7 @@ include("include/menu_gauche.inc.php");
                              <tr>
                                 <td><a href="#">EMAIL</a></td>
                                 <td>
-                                	<input type="text" class="form-control" placeholder="Email" autofocus>
+                                    <input type="text" class="form-control" placeholder="Email" autofocus>
                                 </td>
                                 
                             </tr>
@@ -71,27 +84,27 @@ include("include/menu_gauche.inc.php");
                             <tr>
                                 <td><a href="#">ADRESSE</a></td>
                                 <td>
-                                	 <input type="text" class="form-control" placeholder="Addresse" autofocus>
+                                     <input type="text" class="form-control" placeholder="Addresse" autofocus>
                                 </td>
                                 
                             </tr>
                             <tr>
                                 <td><a href="#">VILLE</a></td>
                                 <td>
-                                	<input type="text" class="form-control" placeholder="Ville" autofocus >
+                                    <input type="text" class="form-control" placeholder="Ville" autofocus >
                                 </td>
                                 
                             </tr>
                             <tr>
                                 <td><a href="#">CODE POSTAL</a></td>
                                 <td>
-                                	<input type="number" class="form-control" placeholder="Code postal" min="0" max="99999" autofocus >
+                                    <input type="number" class="form-control" placeholder="Code postal" min="0" max="99999" autofocus >
                                 </td>
                                 
                             </tr><tr>
                                 <td><a href="#">PAYS</a></td>
                                 <td>
-                                	<select name="pays" class="form-control" id="pays"placeholder="Pays">
+                                    <select name="pays" class="form-control" id="pays"placeholder="Pays">
                 <option value="France">France</option>      
                 <option value="Angleterre">Angleterre</option>  
                 <option value="Irlande">Irlande</option>
@@ -110,53 +123,30 @@ include("include/menu_gauche.inc.php");
                             <tr>
                                 <td><a href="#">DATE DE NAISSANCE</a></td>
                                 <td>
-                                	<input type="date" class="form-control" placeholder="Date" autofocus >
+                                    <input type="date" class="form-control" placeholder="Date" autofocus >
                                 </td>
                                 
                             </tr>
                             <tr>
-                            
-                        </table>
-                        
-                    </div>
-                    <div class="col-lg-6">
-                        
-                        <table class="table">
-                            
-                            
-                            
-                            <tr>
-                                <td><a href="#">PHOTO</a></td>
-                                <td>
-                                </td>
-                                
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="images item " >
-                                        <a href="#myModal" data-toggle="modal">
-                                   <img src="images/imgO1.png" alt="" />
-                               </a>
-                               </div>
-                                    </a>
-
-   
-
-                                </td>
-                                
-                                
-                            </tr>
-                            
-                            <tr>
-                                <td><a href="#">IMAGE DE FOND</a></td>
-                                <td>choix de l'image de fond
-                                </td>
-                                
-                            </tr>
-                            <tr>
+                                <tr>
 
                                 <td><a href="#">RESUME</a></td>
-                                <td>résumé
+                                <td>
+
+                                  
+                                     <br>
+                                      <br>
+
+
+                                    
+                                    <br> <textarea class="wysihtml5 form-control" rows="4"></textarea><br>
+                    
+                                    <div class="bouton">
+
+                                      <center><a href="tableau_de_bord.php">Enregistrer</a></center><br><br>
+                                    </div>
+
+                      
                                 </td>
                                 
                             
@@ -164,34 +154,52 @@ include("include/menu_gauche.inc.php");
                             </tr>
                             <tr>
                                 <td><a href="#">CV</a></td>
-                                <td>cv
+                                <td>
+
+                                  
+                                     <br>
+                                      <br>
+
+
+                                    <div class="boutontab">
+
+                                      <center><a href="#"><i class="fa fa-paperclip"></i></a></center>
+                                    </div>
+                                    <!--<form id="upload" method="post" action="upload.php" enctyper="multipart/form-data"-->
+                                    <br>
+                                    <br>
+                                    
+                                    <div class="bouton">
+
+                                      <center><a href="tableau_de_bord.php">Enregistrer</a></center><br><br>
+                                    </div>
+
+
+
+
                                 </td>
                                 
                             </tr>
-
-                            
-                           
                             
                         </table>
-
                         <div class="bouton">
-                         <center> <a href="modification_information.php"> Enregistrer </a></center>
-                        
+                          <br>
+                          
+
+                         <center> <a href="modification_information_modifs.php"> Modifier </a></center>
+                        <br>
+                          <br>
                         </div>
-            
-        </div>
-        </center>
-        </div>
-        
+                              </div>
+
+                          </div>
+                        </section>
+                </div>
+            </div>
+        </section>
     </section>
-</div>
-</div>
-</section>
-</section>
 
-
-          
-       <?php include('include/right_side_bar.php');
+<?php include('include/right_side_bar.php');
  include('include/js.inc.php'); ?>
 
 </body>
