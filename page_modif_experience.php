e9b59ed49c3e0ffdced8966b03ab03d87d4a986d
charlotte decary <charlottedecary@MacBook-de-charlotte-2.local>
2018-05-02 17:55:04 +0200

    egr
    erg

---
+++ page_modif_experience.php
@@ -0,0 +1,103 @@
+<?php 
+$title="Expérience Modification";
+include("include/header.inc.php"); 
+include("include/menu_haut.inc.php");
+include("include/menu_gauche.inc.php");
+?>
+
+<body>
+    
+    <section id="main-content">
+        <section class="wrapper">
+        <!-- page start-->
+
+        <div class="row">
+                <div class="col-md-12">
+                    <!--breadcrumbs start -->
+                    <ul class="breadcrumb">
+                        <li><a href="tableau_de_bord.php"><i class="fa fa-home"></i> Tableau de bord</a></li>
+                        <li class="active">Modification Experience</li>
+                    </ul>
+                    <!--breadcrumbs end -->
+                </div>
+            </div>
+            <div class="row">
+
+              <div class="col-lg-12">
+                <section class="panel">
+                  <header class="panel-heading">
+                    Experience 
+
+                  </header>
+                  <div class="panel-body">
+                  	<table class="table">
+                            <thead>
+                            <tr>
+                                <th> ECE PARIS</th>
+                                <th>Ecole d'ingénieur</th>
+                                <th>2015 - 2017</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td><a href="#">Domaine</a></td>
+                                <td>Ingienerie</td>    
+                                
+                            </tr>
+                            <tr>
+                                <td><a href="#">Associations</a></td>
+                                <td>SDI, Les Caves, UPA</td>    
+                                
+                            </tr>
+                            <tr>
+                                <td><a href="#">Description</a></td>
+                                <td>Majeur: Ocres, Mineur: Ingiénérie d'affaire</td>    
+                                
+                            </tr>
+                        </tbody>
+                            <thead>
+                            <tr>
+                                <th> <br> <br> SJH</th>
+                                <th>Lycée</th>
+                                <th>2009 - 2015</th>
+                                <th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td><a href="#">Domaine</a></td>
+                                <td>Bac scientifique</td>    
+                                
+                            </tr>
+                            <tr>
+                                <td><a href="#">Associations</a></td>
+                                <td> Soeurs de la miséricorde</td>    
+                                
+                            </tr>
+                            <tr>
+                                <td><a href="#">Description</a></td>
+                                <td> Bac mention bien </td>    
+                                
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="bouton">
+                         <center> <a href="experience.php"> Enregistrer </a></center>
+                        
+                        </div>
+
+
+                  </div>
+              </section>
+          </div>
+          
+      </div>
+  </section>
+</section>
+
+
+<?php include('include/right_side_bar.php');
+ include('include/js.inc.php'); ?>
+
+</body>
+</html>
