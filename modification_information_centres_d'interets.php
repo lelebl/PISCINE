c50b4c1e903cd9b746f592a5261d33d63f09b068
charlotte decary <charlottedecary@MacBook-de-charlotte-2.local>
2018-05-02 16:00:09 +0200

    motifs centre d'interet
    creation page d’accès a la modification des centres d’interets

---
+++ modification_information_centres_d'interets.php
@@ -0,0 +1,120 @@
+<?php 
+$title="Modification Information Centres d'interets";
+include("include/header.inc.php"); 
+include("include/menu_haut.inc.php");
+include("include/menu_gauche.inc.php");
+?>
+
+<body>
+    <!--main content start-->
+    <section id="main-content" >
+    <section class="wrapper">
+            <div class="row">
+                <div class="col-md-12">
+                    <!--breadcrumbs start -->
+                    <ul class="breadcrumb">
+                        <li><a href="accueil.php"><i class="fa fa-home"></i> Accueil</a></li>
+                        <li><a href="modification_information.php"></i> Informations personnelles</a></li>
+                        <li class="active">Modification Centres d'intérêts</li>
+                    </ul>
+                    <!--breadcrumbs end -->
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4">
+                        <section class="panel">
+                            <div class="panel-heading">
+                                Informations du profil
+                            </div>
+                                <div class="panel-body">
+            
+                                    
+                                        <a href="modification_information_modifs.php" class="btn btn-compose">
+                                            Informations personnelles
+                                        </a><br><br>
+
+                                        <a href="modification_information_compétences.php" class="btn btn-compose">
+                                            Compétences
+                                        </a><br><br>
+                                        <a href="modification_information_centres_d'interets.php" class="btn btn-compose">
+                                            Centre d'intérêts
+                                        </a>
+                                </div>
+                        </section>
+                </div>
+                
+            
+                <div class="col-lg-8">
+                        <section class="panel">
+                    <header class="panel-heading">
+                        Centres d'intérêts
+                        <span class="tools pull-right">
+                            <a href="javascript:;" class="fa fa-chevron-down"></a>
+                         </span>
+                    </header>
+                    <div class="panel-body">
+                        <table class="table  table-hover general-table">
+                            <thead>
+                              <tr>
+                                <th> Quels sont vos hobbies?</th>
+                                <th>          </th>
+                            </tr>
+
+                            </thead>
+                           <tbody>
+                            <tr>
+                                 <td><a href="#">Sport</a></td>
+                                 <td>
+                                  <span class="tools pull-right">
+                                    <a href="javascript:;" class="fa fa-times"></a>
+                                  </span>
+                                </td>
+                          </tr>
+                            <tr>
+                                <td><a href="#">Musique</a></td>
+                                <td>
+                                  <span class="tools pull-right">
+                                    <a href="javascript:;" class="fa fa-times"></a>
+                                  </span>
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <td><a href="#">Voyage</a></td>
+                                <td>
+                                  <span class="tools pull-right">
+                                    <a href="javascript:;" class="fa fa-times"></a>
+                                  </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><a href="#">Cuisine</a></td>
+                                <td>
+                                  <span class="tools pull-right">
+                                    <a href="javascript:;" class="fa fa-times"></a>
+                                  </span>
+                                </td>
+                            </tr>
+                            </tbody> 
+                          
+                        </table>
+                        <div class="bouton">
+                         <center> <a href="page_modif_infos.php"> Modifier </a></center>
+                        
+                        </div>
+                    </div>
+                    </div>
+                </section>
+                </div>
+            </div>
+
+            </div>
+        </section>
+    </section>
+
+<?php include('include/right_side_bar.php');
+ include('include/js.inc.php'); ?>
+
+</body>
+</html>
