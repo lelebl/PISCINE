c0d0871eb1a8d54f0248f1c7898e9b8ee8b712b9
EmmanuelleTHRLX <emmanuelle.thiroloix@gmail.com>
2018-05-05 22:13:45 +0200

    Modif beug

--- tableau_de_bord.php
+++ tableau_de_bord.php
@@ -11,16 +11,9 @@ include("include/menu_gauche.inc.php");
 
 
 
-$texte = $_GET['texte'];
-$lieu = $_GET['lieu'];
-$fichier = $_GET['fichier'];
-$sentiment = $_GET['sentiment'];
-$heure = $_GET['heure'];
-
-
 define('DB_SERVER','localhost');
 define('DB_USER','root');
-define('DB_PASS','root');
+define('DB_PASS','');
 
 $database = "piscine";
 
