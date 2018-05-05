dcdd1ef908ab3bbe405ff606ae0bb87c12b101bc
EmmanuelleTHRLX <emmanuelle.thiroloix@gmail.com>
2018-05-05 19:57:36 +0200

    TROP STYLE

--- ajouter.php
+++ ajouter.php
@@ -1,15 +1,19 @@
 <?php
-require 'include/functions.php';
-include("include/header.inc.php"); 
-include("include/menu_haut.inc.php"); 
-include("include/menu_gauche.inc.php"); 
+
+include("include/db.php");
+session_start();
+
 
 $id_ajout=$_GET['id'];
-$username_ajout=$_GET['username_ajout'];
+$username_ajout=$_GET['nom'];
+$prenom=$_GET['prenom'];
 
 require_once 'include/db.php';
 
 $req = $pdo->prepare("INSERT INTO relations SET id_1 = ?, id_2 = ?");
 $req->execute([$_SESSION['auth']->id,$id_ajout]);
+$_SESSION['interraction']['success'] = $username_ajout.' '.$prenom.' a bien été ajouté';
+header('Location: ajouter_qq.php');
+exit();
 
 ?>
